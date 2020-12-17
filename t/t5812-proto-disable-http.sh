a575cdce29e1752fe3b12b5e7a1d48be63e4a22e
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-12-18 00:38:51 +0100

    fixup! t58*: make them independent of the default branch name

--- t/t5812-proto-disable-http.sh
+++ t/t5812-proto-disable-http.sh
@@ -9,7 +9,8 @@ start_httpd
 test_expect_success 'create git-accessible repo' '
 	bare="$HTTPD_DOCUMENT_ROOT_PATH/repo.git" &&
 	test_commit one &&
-	git --bare init "$bare" &&
+	git branch -M main &&
+	git --bare init -b main "$bare" &&
 	git push "$bare" HEAD &&
 	git -C "$bare" config http.receivepack true
 '
