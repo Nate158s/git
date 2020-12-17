71ba9a3c347b1e8a5fa980a152d02314ccdb30f5
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-12-17 23:39:32 +0100

    fixup! TODO: these tests still rely on `master` being the default branch name

--- t/t3407-rebase-abort.sh
+++ t/t3407-rebase-abort.sh
@@ -2,9 +2,6 @@
 
 test_description='git rebase --abort tests'
 
-GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=master
-export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
-
 . ./test-lib.sh
 
 ### Test that we handle space characters properly
